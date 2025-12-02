bcd0d7be7345d84d4cc1c1c2b54c96814985e9d4
Victor Villa <victor.villa.jimenez@gmail.com>
2025-12-02 01:12:01 -0800

    Prettier updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [refresh, setRefresh] = useState(3);
   const [iot, setIot] = useState([]);
   const [inputs, setInputs] = useState([]);
   const [discovering, setDiscovering] = useState(false);
 
   const fetchIoTs = async () => {
-    const response = await fetch('http://localhost:8080/api/discover', {
-      method: 'GET',
-      mode: 'cors',
+    const response = await fetch("http://localhost:8080/api/discover", {
+      method: "GET",
+      mode: "cors",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     });
     // console.log(response, data);
     const data = await response.json();
     const things = data?.iot || [];
     let inputData = [];
     if (inputs && inputs.length) {
-      inputData = things.map(({topic, desirable}) => {
-        const idx = inputs.findIndex(t => t.topic === topic);
+      inputData = things.map(({ topic, desirable }) => {
+        const idx = inputs.findIndex((t) => t.topic === topic);
         if (idx !== -1 && inputs[idx].desirable !== desirable) {
           return { topic, desirable: inputs[idx].desirable };
         }
-        return { topic, desirable }
+        return { topic, desirable };
       });
     } else {
-      inputData = things.map(({topic, desirable}) => { return { topic, desirable }; });  
+      inputData = things.map(({ topic, desirable }) => {
+        return { topic, desirable };
+      });
     }
     setInputs(inputData);
     setIot(things);
@@ -50,31 +52,31 @@ function App() {
   }, [refresh, inputs]);
 
   const updateThing = async (thing) => {
-    await fetch('http://localhost:8080/api/message', {
-      method: 'POST',
-      mode: 'cors',
+    await fetch("http://localhost:8080/api/message", {
+      method: "POST",
+      mode: "cors",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      body: JSON.stringify(thing)
-    })
-  }
+      body: JSON.stringify(thing),
+    });
+  };
 
   const onChangeHandle = (e) => {
     const topic = e.target.dataset.topic;
-    const idx = inputs.findIndex(t => t.topic === topic);
+    const idx = inputs.findIndex((t) => t.topic === topic);
     const thing = inputs[idx];
     const newThing = Object.assign({}, thing);
     newThing.desirable = e.target.value;
     setInputs((oldinputs) => oldinputs.splice(idx, 1, newThing));
   };
-  
+
   const onClickHandle = async (e) => {
     const topic = e.target.dataset.topic;
-    const idx = inputs.findIndex(t => t.topic === topic);
+    const idx = inputs.findIndex((t) => t.topic === topic);
     const thing = inputs[idx];
-    await updateThing(thing)
+    await updateThing(thing);
   };
 
   const onChangeRefresh = (e) => {
@@ -88,53 +90,65 @@ function App() {
       </header>
       <div>
         <div className="sidebar">
-          <h2>Team 35</h2>
+          <h2>Team X</h2>
           <ul>
-            <li>Jose Victor Villa Jimenez</li>
-            <li>Carlos Sevilla Silva</li>
-            <li>Alan Galeana Vega</li>
+            <li>Victor Villa</li>
+            <li>...</li>
+            <li>...</li>
           </ul>
         </div>
         <main>
           <div className="info">
             <div>
               <label htmlFor="refresh">Refresh (in seconds): </label>
-              <select id="refresh" name="refresh" value={refresh} onChange={onChangeRefresh}>
-                {Array(10).fill(0).map((_, i) => 
-                  <option
-                    key={i}
-                    value={i + 1}>
+              <select
+                id="refresh"
+                name="refresh"
+                value={refresh}
+                onChange={onChangeRefresh}
+              >
+                {Array(10)
+                  .fill(0)
+                  .map((_, i) => (
+                    <option key={i} value={i + 1}>
                       {i + 1}
-                  </option>)}
+                    </option>
+                  ))}
               </select>
-          </div>
+            </div>
           </div>
           <div className="card">
-            {iot && inputs && inputs.length === iot.length && iot.map((v, i) =>
-              <section key={v.topic} >
-                <p>Thing: {v.topic}</p>
-                <p>current value: {v.value}</p>
-                <input
-                  type='number'
-                  value={inputs[i].desirable}
-                  data-topic={inputs[i].topic}
-                  onChange={onChangeHandle} />
-                <button
-                  type='button'
-                  data-topic={inputs[i].topic}
-                  onClick={onClickHandle}>
+            {iot &&
+              inputs &&
+              inputs.length === iot.length &&
+              iot.map((v, i) => (
+                <section key={v.topic}>
+                  <p>Thing: {v.topic}</p>
+                  <p>current value: {v.value}</p>
+                  <input
+                    type="number"
+                    value={inputs[i].desirable}
+                    data-topic={inputs[i].topic}
+                    onChange={onChangeHandle}
+                  />
+                  <button
+                    type="button"
+                    data-topic={inputs[i].topic}
+                    onClick={onClickHandle}
+                  >
                     Update
-                </button>
-              </section>
-            )}
+                  </button>
+                </section>
+              ))}
           </div>
           <p className="read-the-docs">
-            HiveMQ Cloud, ExpressJS, ViteJS, ReactJS, Wokwi Simulator, VS Code PlatformIO
+            HiveMQ Cloud, ExpressJS, ViteJS, ReactJS, Wokwi Simulator,
+            PlatformIO, VS Code
           </p>
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
